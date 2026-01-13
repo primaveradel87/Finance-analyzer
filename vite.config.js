19870195715e1c58999559834716838c5dcbef76
primaveradel87 <santiago@crezes.com>
2026-01-12 18:23:12 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        // Divide las librerías grandes para mejorar la velocidad de carga
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('pdfjs-dist')) return 'vendor-pdfjs';
+            if (id.includes('recharts')) return 'vendor-charts';
+            return 'vendor'; // Resto de dependencias
+          }
+        },
+      },
+    },
   },
   server: {
     port: 3000,
